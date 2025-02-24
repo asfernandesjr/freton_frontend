6b24070063602a99deef0246c1173ae9c820d044
Antonio Fernandes <antonio.fernandes@livon.io>
2025-02-24 13:15:36 -0300

    tailwind 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/eslint-module'],
+  modules: ['nuxt-icon', '@nuxtjs/eslint-module'],
   typescript: {
     typeCheck: true,
   },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()],
+  }
 });
